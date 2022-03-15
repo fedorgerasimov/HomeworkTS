f9431e08432bcf4e97120bdb10096e1bb64480e8
fedorgerasimov <1031577@mail.ru>
2022-03-15 14:39:51 +0300

    file HW2 is done. Added type and pure helper functions

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -1,13 +1,18 @@
 import React, {useState} from 'react'
 import Affairs from './Affairs'
 
+
 // types
-export type AffairPriorityType = any // need to fix any
-export type AffairType = any // need to fix any
-export type FilterType = 'all' | AffairPriorityType
+export type AffairPriorityType = 'low' | 'middle' | 'high' // need to fix any - тип приоритетов
+export type AffairType = {  //any // need to fix any
+    _id: number
+    name: string
+    priority: string
+}
+export type FilterType = 'all' | AffairPriorityType  // у фильтра все типы Affair приоритета + all
 
 // constants
-const defaultAffairs: any = [ // need to fix any
+const defaultAffairs: Array<AffairType> = [ // need to fix any
     {_id: 1, name: 'React', priority: 'high'},
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
@@ -15,21 +20,29 @@ const defaultAffairs: any = [ // need to fix any
     {_id: 5, name: 'html & css', priority: 'middle'},
 ]
 
-// pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-    if (filter === 'all') return affairs
-    else return // need to fix
+// pure helper functions чистые функции
+export const filterAffairs = (affairs: Array<AffairType>, filter: string): Array<AffairType> => { // need to fix any фильтрация массива
+    if (filter === 'all') {     // need to fix
+        return affairs
+    } else if (filter === 'low') {
+        return affairs.filter((element) => element.priority === 'low')
+    } else if (filter === 'middle') {
+        return affairs.filter((element) => element.priority === 'middle')
+    } else {
+        return affairs.filter((element) => element.priority === 'high')
+    }
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-    return // need to fix
+export const deleteAffair = (affairs: Array<AffairType>, _id: number): Array<AffairType> => { // need to fix any
+    return affairs.filter((element) => element._id !== _id)    // Возвращаем массив дел, но без нужного. Если пришла 1, возвращаем массив дел без _id: 1
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
-    const [filter, setFilter] = useState<FilterType>('all')
+    const [affairs, setAffairs] = useState<Array<AffairType>>(defaultAffairs) // need to fix any - указываем тип массива. Храним defaultAffairs array
+    const [filter, setFilter] = useState<FilterType>('all')   // храним фильтр, вначале all. Когда кликаем по кнопке, меняем на 'low' | 'middle' | 'high'
 
-    const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => setAffairs(deleteAffair(affairs, _id)) // need to fix any
+    const filteredAffairs = filterAffairs(affairs, filter)  //Создали временную переменную. Нужна для хранения отфильтрованного массива (фильтр был выбран). Затем передаём в следующую компоненту
+    //пропускаем affairs через чистую функцию filterAffairs, чтобы она отфильтровала не все элементы, а только нужные. Не сетаем, так как потеряем весь массив
+    const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id)) // need to fix any  default array меняем когда нам нужно что-либо удалить
 
     return (
         <div>
