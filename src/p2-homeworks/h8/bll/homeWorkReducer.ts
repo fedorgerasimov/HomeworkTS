b73b3df8022bf4f666412c298a0a4ba5a6743367
fedorgerasimov <1031577@mail.ru>
2022-07-07 16:53:08 +0300

    H8, added CSS

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,28 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type SortUpAT = { type: 'sort', payload: 'up' }
+type SortDownAT = { type: 'sort', payload: 'down' }
+type CheckAT = { type: 'check', payload: number }
+type ActionType = SortUpAT | SortDownAT | CheckAT
+/*type ActionType = {type: 'sort', payload: 'up' | 'down'} | {type: 'check', payload: number}*/
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            const newState = [...state].sort((a, b) =>
+                    a.name < b.name ? -1 : 1
+                /* { if (a.name > b.name) return 1
+                 else if(a.name < b.name) return -1
+                 else return 0}*/ // лучше использовать этот вариант с проверкой на 0, когда имена могут совпадать
+            )
+            return action.payload === 'up' ? newState : newState.reverse()
+            //уже приходит отсортированный массив и проверяем
+            // если в функции передаём up возвращаем массив, если другое - reverse()
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(el => el.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
