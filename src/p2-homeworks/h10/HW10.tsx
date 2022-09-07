9af8233a81cd54045bc82b15fec041f890b2c2f1
fedorgerasimov <1031577@mail.ru>
2022-09-07 19:02:37 +0300

    H10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
+        dispatch(loadingAC(true))
+        setTimeout(()=> {
+           dispatch( loadingAC(false))
+        }, 2000)
+        //console.log('loading...')
     };
 
     return (
@@ -17,12 +22,12 @@ function HW10() {
             homeworks 10
 
             {/*should work (должно работать)*/}
-            {loading
+            {isLoading
                 ? (
-                    <div>крутилка...</div>
+                    <div><img src={require('./Ellipsis-1.4s-223px.gif')} style={{height: '3.4em'}}/></div>
                 ) : (
                     <div>
-                        <SuperButton onClick={setLoading}>set loading...</SuperButton>
+                        <SuperButton style={{width: '130px', margin: '24px 0px 1px 10px'}} onClick={setLoading}>set loading...</SuperButton>
                     </div>
                 )
             }
@@ -31,6 +36,7 @@ function HW10() {
             {/*для личного творчества, могу проверить*/}
             {/*<Alternative/>*/}
             <hr/>
+
         </div>
     )
 }
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,19 @@
-const initState = {
+/*export type InitialStateType = {
+    isLoading: boolean
+}*/
+export type InitialStateType = typeof initState // автоматическая типизация
 
+const initState = {  // показывать isLoading
+    isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionType): InitialStateType => { // fix any
     switch (action.type) {
-        case '': {
+        case 'TOGGLE_IS_LOADING':
+            return {...state, isLoading: action.isLoading}
+        default:
             return state
-        }
-        default: return state
     }
 }
-
-export const loadingAC = (): any => {} // fix any
+export type ActionType = ReturnType<typeof loadingAC> // автоматическая типизация
+export const loadingAC = (isLoading: boolean) => ({type: "TOGGLE_IS_LOADING", isLoading} as const) // fix any
